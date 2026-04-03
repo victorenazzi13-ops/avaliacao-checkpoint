145df78f151e66a5a85c3bc65aae40a48c04bd2c
Denisson Victor S. Santana <victorenazzi13@gmail.com>
2026-04-03 15:40:22 -0300

    fix: ajusta initial screen conforme modelo da aula

--- loja_app/lib/src/screens/initial_screen.dart
+++ loja_app/lib/src/screens/initial_screen.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import '../widgets/hero_section_widget.dart';
 import '../widgets/product_card_widget.dart';
 import '../data/produtos.dart';
 
-class InitialScreen extends StatelessWidget {
+class InitialScreen extends StatefulWidget {
   const InitialScreen({super.key});
 
   @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Minha Loja"), centerTitle: true),
+      appBar: AppBar(
+        leading: const Icon(Icons.menu, size: 40),
+        title: Image.asset('assets/logo_usedev.png', height: 40),
+        centerTitle: true,
+        actions: const [
+          Icon(Icons.person_outline, size: 40),
+          SizedBox(width: 10),
+          Icon(Icons.shopping_cart_outlined, size: 40),
+          SizedBox(width: 25),
+        ],
+      ),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -29,21 +45,16 @@ class InitialScreen extends StatelessWidget {
                 ),
               ),
             ),
-
             const HeroSection(),
-
-            const SizedBox(height: 20),
-
-            const Padding(
-              padding: EdgeInsets.all(16),
-              child: Text(
-                'Promoções Especiais',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            Text(
+              'Promos Especiais',
+              style: TextStyle(
+                fontFamily: GoogleFonts.orbitron().fontFamily,
+                fontSize: 27,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
               ),
             ),
-
-            const SizedBox(height: 10),
-
             ListView.builder(
               shrinkWrap: true,
               physics: const NeverScrollableScrollPhysics(),
